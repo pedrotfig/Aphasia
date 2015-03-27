87e31d3aed7c14baa076d2a7b1ae23a3d90fca5c
JuSalgado <jsalgado@MacBook-Pro-de-Juliana.local>
2015-03-27 16:14:00 -0300

    BoardDetail

---
+++ CategoriesViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  Aphasia
+//
+//  Created by Juliana Salgado on 27/03/15.
+//  Copyright (c) 2015 B-B-BOOM!. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CategoriesViewController : UITableViewController
+
+@end
