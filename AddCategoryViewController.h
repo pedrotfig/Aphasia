3e980cfc9b74495cdc2ffdbc308497c16dac60c8
Elias Ayache <elias_ayache@hotmail.com>
2015-04-01 17:42:18 -0300

    fix error

--- AddCategoryViewController.h
+++ AddCategoryViewController.h
@@ -7,9 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
 #import "StoredData.h"
 
-@interface AddCategoryViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate>{
+@interface AddCategoryViewController : UIViewController <UINavigationControllerDelegate, UIImagePickerControllerDelegate, AVAudioRecorderDelegate, AVAudioPlayerDelegate>{
     IBOutlet UITextField *titleCategory;
     UIImage *addPhotoCategory;
 }
@@ -17,8 +18,11 @@
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIButton *recordButton;
 @property (weak, nonatomic) IBOutlet UIButton *stopButton;
+@property (weak, nonatomic) IBOutlet UIButton *playButton;
 
 - (IBAction)recordTapped:(id)sender;
 - (IBAction)stopTapped:(id)sender;
+- (IBAction)playTapped:(id)sender;
+
 
 @end
