3636ac88dfc64ddba3af4f6b9a95359418890ea3
Juliana Salgado <juju-salgado@hotmail.com>
2015-04-02 11:02:23 -0300

    audio

--- AddCategoryViewController.m
+++ AddCategoryViewController.m
@@ -13,14 +13,19 @@
 
 @property (strong, nonatomic) UIImagePickerController *picker;
 
+@property (strong, nonatomic) NSString *imageAdded;
+
+@property (strong, nonatomic) NSString *audioAdded;
 
 @end
 
 @implementation AddCategoryViewController
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //allow to edit image
     [self.imageView setImage:[UIImage imageNamed:@"addphoto"]];
     [self.imageView setUserInteractionEnabled:YES];
     UITapGestureRecognizer *singleTap =  [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(singleTapping:)];
@@ -33,7 +38,43 @@
     [self.picker setDelegate:self];
     addPhotoCategory = [UIImage imageNamed:@"customPhoto"];
     
+    //just show the button when start is pressed
     self.stopButton.hidden = YES;
+    
+    //allow to save audio
+    
+    // Set the audio file
+    NSArray *pathComponents = [NSArray arrayWithObjects:
+                               [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],
+                               @"MyAudioMemo.m4a",
+                               nil];
+    NSURL *outputFileURL = [NSURL fileURLWithPathComponents:pathComponents];
+    
+    NSError *error = nil;
+    // Setup audio session
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    [session setCategory:AVAudioSessionCategoryPlayAndRecord error:nil];
+    
+    // Define the recorder setting
+    NSMutableDictionary *recordSetting = [[NSMutableDictionary alloc] init];
+    
+    [recordSetting setValue:[NSNumber numberWithInt:kAudioFormatMPEG4AAC] forKey:AVFormatIDKey];
+    [recordSetting setValue:[NSNumber numberWithFloat:44100.0] forKey:AVSampleRateKey];
+    [recordSetting setValue:[NSNumber numberWithInt: 2] forKey:AVNumberOfChannelsKey];
+    
+    // Initiate and prepare the recorder
+    recorder = [[AVAudioRecorder alloc] initWithURL:outputFileURL settings:recordSetting error:&error];
+    recorder.delegate = self;
+    recorder.meteringEnabled = YES;
+ 
+ 
+    //TODO future handle error
+    if (error)
+    {
+        NSLog(@"error: %@", [error localizedDescription]);
+    } else {
+        [recorder prepareToRecord];
+    }
     
     NSArray *dirPaths;
     NSString *docsDir;
@@ -41,51 +82,15 @@
     dirPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
     docsDir = dirPaths[0];
     
-    NSString *soundFilePath = [docsDir stringByAppendingPathComponent:@"audio"];
-    
-    NSURL *soundFileURL = [NSURL fileURLWithPath:soundFilePath];
-    
-    NSDictionary *recordSettings = [NSDictionary
-                                    dictionaryWithObjectsAndKeys:
-                                    [NSNumber numberWithInt:AVAudioQualityMin],
-                                    AVEncoderAudioQualityKey,
-                                    [NSNumber numberWithInt:16],
-                                    AVEncoderBitRateKey,
-                                    [NSNumber numberWithInt: 2],
-                                    AVNumberOfChannelsKey,
-                                    [NSNumber numberWithFloat:44100.0],
-                                    AVSampleRateKey,
-                                    nil];
-    
-    NSError *error = nil;
-    
-    AVAudioSession *audioSession = [AVAudioSession sharedInstance];
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
-                        error:nil];
-    
-    recorder = [[AVAudioRecorder alloc]
-                      initWithURL:soundFileURL
-                      settings:recordSettings
-                      error:&error];
-    
-    if (error)
-    {
-        NSLog(@"error: %@", [error localizedDescription]);
-    } else {
-        [recorder prepareToRecord];
-    }    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)saveNewCategory:(id)sender {
-    [StoredData addCategoryWithName:titleCategory.text andImage:@"customPhoto" andAudio:@"audio" andRelations:nil];
-    [self.navigationController popViewControllerAnimated:TRUE];
-}
+#pragma mark images
 
+//Gesture of changing picture
 -(void)singleTapping:(UIGestureRecognizer *)recognizer
 {
     UIAlertView *alert = [[UIAlertView alloc]
@@ -97,6 +102,7 @@
     [alert show];
 }
 
+//handle option of change image
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
     if (buttonIndex == 1) {
         NSLog(@"Camera");
@@ -106,23 +112,29 @@
     }
     if (buttonIndex == 2) {
         NSLog(@"Photo Library");
-        [self.picker setSourceType:UIImagePickerControllerSourceTypePhotoLibrary];
+        
+        [self.picker setSourceType:UIImagePickerControllerSourceTypeSavedPhotosAlbum];
         [self presentViewController:self.picker animated:YES completion:nil];
     }
     
     
 }
 
-
 -(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info{
     addPhotoCategory = [info objectForKey:@"UIImagePickerControllerOriginalImage"];
     [self.imageView setImage:addPhotoCategory];
+    NSURL *imagePath = [info objectForKey:@"UIImagePickerControllerReferenceURL"];
+    self.imageAdded = [imagePath lastPathComponent];
+    
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 -(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
     [self dismissViewControllerAnimated:YES completion:nil];
 }
+
+#pragma mark audio
+
 
 - (IBAction)recordTapped:(id)sender {
     if (!recorder.recording)
@@ -133,6 +145,16 @@
     }
 }
 
+//Starts recording an audio
+- (IBAction)playButtonTapped:(id)sender {
+    if (!recorder.recording){
+        player = [[AVAudioPlayer alloc] initWithContentsOfURL:recorder.url error:nil];
+        [player setDelegate:self];
+        [player play];
+    }
+}
+
+//Stops recording an audio
 - (IBAction)stopTapped:(id)sender {
     self.stopButton.hidden = YES;
     self.recordButton.hidden = NO;
@@ -146,6 +168,9 @@
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
         [alert show];
+        
+        //store record
+        
     }
 }
 
@@ -163,8 +188,11 @@
     NSLog(@"Decode Error occurred");
 }
 
+//handle the completion of recording
 -(void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder successfully:(BOOL)flag
 {
+    self.stopButton.hidden = YES;
+    self.recordButton.hidden = NO;
 }
 
 -(void)audioRecorderEncodeErrorDidOccur:(AVAudioRecorder *)recorder error:(NSError *)error
@@ -172,22 +200,13 @@
     NSLog(@"Encode Error occurred");
 }
 
+#pragma mark save
 
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
-/*
- TODO: store new category on scene transition
- To store new category, use the command:
- 
- [StoredData addCategoryWithName:titleCategory.text andImage:@"category_emotion" andAudio:@"audio0"];
- */
+//Store new category
+- (IBAction)saveNewCategory:(id)sender {
+    [StoredData addCategoryWithName:titleCategory.text andImage:self.imageAdded andAudio:@"audio" andRelations:nil];
+    [self.navigationController popViewControllerAnimated:TRUE];
+}
 
 @end
